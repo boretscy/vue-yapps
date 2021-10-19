0d20b52d6ceaf61ad733d6dc2da1a2429d8c6a4a
boretscy <boretscy@gmail.com>
2021-10-19 14:18:45 +0300

    цели и заявки в коллтач

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -308,8 +308,7 @@ export default {
                 console.log(response);
                 console.log(window.dataLayer);
 
-
-                this.PushGoal({
+                let GoalData = {
 
                     Category: 'Виджеты',
 					IDToneCategory: Widget.IDToneCategory,
@@ -326,7 +325,28 @@ export default {
                         CTSession: Form.SendData.CTSession
 
 					}
-                });
+                };
+
+                // Metrics
+                if ( typeof window.dataLayer != 'undefined' ) window.dataLayer.push({'event': 'FormSubmission', 'eventCategory': GoalData.IDToneCategory, 'eventAction': 'submit' });
+                console.log( window.dataLayer );
+                if ( typeof window.Matomo != 'undefined' ) window._paq.push(["trackEvent", GoalData.Category, GoalData.Action, GoalData.Name]);
+                if ( typeof window[GoalData.YandexCounter] != 'undefined' && typeof GoalData.Yandex != 'undefined' ) window[GoalData.YandexCounter].reachGoal(GoalData.Yandex);
+                
+                // CallTouch
+                if ( GoalData.CallTouch.Flag ) { 
+                
+                    let CallTouchURL = 'https://api.calltouch.ru/calls-service/RestAPI/requests/'+GoalData.CallTouch.CTSiteID+'/register/';
+                    CallTouchURL += '?subject='+encodeURIComponent(GoalData.Name)+' '+encodeURIComponent(GoalData.Action);
+                    CallTouchURL += '&sessionId='+window['call_value_'+GoalData.CallTouch.CTSession];
+                    CallTouchURL += '&phoneNumber='+GoalData.CallTouch.Phone.replace(/[^\d;]/g, '');
+                    if ( GoalData.CallTouch.Name ) CallTouchURL += '&fio='+encodeURIComponent(GoalData.CallTouch.Name);
+                    CallTouchURL += '&requestUrl='+location.href;
+                    
+                    let request = new XMLHttpRequest();
+                    request.open('GET', CallTouchURL, true);
+                    request.send();
+                }
             })
             .catch(function (error) {
                 console.log(error);
