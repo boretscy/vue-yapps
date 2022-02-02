a34b2c14b61e717ef59328f56299405fe0d85bc6
boretscy <boretscy@gmail.com>
2022-02-02 11:13:47 +0300

    u

--- src/components/Widgets/WidgetLG.vue
+++ src/components/Widgets/WidgetLG.vue
@@ -145,15 +145,13 @@ export default {
     height: 150px;
     overflow: hidden;
 }
-.YApps_Widget--Text p {
+.YApps_Widget--Content .YApps_Widget--Text p {
     margin: 0 0 15px;
 }
-.YApps_Widget--Text ul {
+.YApps_Widget--Content .YApps_Widget--Text ul {
     margin: 15px 0;
     padding: 0 0 0 15px;
-}
-.YApps_Widget--Text ul li {
-    list-style: disc;
+    list-style-type: disc;
 }
 
 @media (max-width: 767px) {
